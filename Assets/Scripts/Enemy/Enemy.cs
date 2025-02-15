b53236572ff23a05d1e9374e37c1c12185fa2a44
hyeonnnnn <paige4157@gmail.com>
2025-02-15 12:21:26 +0900

    Feat: Enemy Detect Player

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,13 +1,19 @@
 using UnityEngine;
+using UnityEditor;
 
 public class Enemy : MonoBehaviour
 {
     public EnemyData enemyData;
-    private float sightRange = 120f;
+    private const float fovAngle = 120f;
+    private bool isDetected;
+
+    Color _blue = new Color(0f, 0f, 1f, 0.2f);
+    Color _red = new Color(1f, 0f, 0f, 0.2f);
 
     private void Update()
     {
         Move();
+        CheckDetectPlayer();
     }
 
     private void Move()
@@ -15,9 +21,32 @@ public class Enemy : MonoBehaviour
         transform.Translate(Vector3.forward * enemyData.speed * Time.deltaTime);
     }
 
-    private void DetectPlayer()
+    private void CheckDetectPlayer()
     {
+        GameObject player = GameObject.FindWithTag("Player");
 
+        if (player != null)
+        {
+            Vector3 directionToPlayer = (player.transform.position - transform.position).normalized;
+            float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+            float verticalAngle = Vector3.Angle(transform.forward, directionToPlayer);
+
+            if (distanceToPlayer <= enemyData.sightRange && verticalAngle <= fovAngle / 2)
+            {
+                isDetected = true;
+            }
+            else
+            {
+                isDetected = false;
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Handles.color = isDetected ? _red : _blue;
+        Handles.DrawSolidArc(transform.position, transform.right, transform.forward, fovAngle / 2, enemyData.sightRange);
+        Handles.DrawSolidArc(transform.position, transform.right, transform.forward, -fovAngle / 2, enemyData.sightRange);
     }
 
     private void chasePlayer()
